58cb10a1369282c60839e288275c0be41bf87e4e
Shaiju Edakulangara <eshaiju@gmail.com>
2023-10-17 19:42:49 +0530

    Add merchant oder query service

---
+++ app/services/order_query_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class OrderQueryService
+  attr_reader :merchant_id
+
+  def initialize(merchant_id)
+    @merchant_id = merchant_id
+  end
+
+  def orders(date, date_week_ago)
+    Order.select(:id, :amount)
+         .joins(:merchant)
+         .where(merchant_id: merchant_id)
+         .where(<<~SQL.squish, date: date, date_week_ago: date_week_ago)
+           (disbursement_frequency = 'daily' AND order_date = :date) OR
+           (disbursement_frequency = 'weekly' AND
+             EXTRACT(DOW FROM live_on) = EXTRACT(DOW FROM TIMESTAMP :date) AND
+             order_date BETWEEN :date_week_ago AND :date)
+         SQL
+  end
+end
---
+++ spec/services/order_query_service_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe OrderQueryService, type: :service do
+  subject(:order_query_service) { described_class.new(merchant.id) }
+
+  let(:merchant) { create(:merchant) }
+  let(:date) { Date.current }
+  let(:date_week_ago) { date - 7.days }
+
+  describe '#orders' do
+    it 'returns daily orders for a merchant' do
+      daily_order = create(:order, merchant: merchant, order_date: date)
+
+      result = order_query_service.orders(date, date_week_ago)
+
+      expect(result).to include(daily_order)
+    end
+
+    it 'returns weekly orders for a merchant' do
+      merchant.update!(disbursement_frequency: 'weekly')
+      weekly_order = create(:order, merchant: merchant, order_date: date)
+
+      result = order_query_service.orders(date, date_week_ago)
+
+      expect(result).to include(weekly_order)
+    end
+
+    it 'does not return orders for other merchants' do
+      other_merchant = create(:second_merchant)
+      create(:order, merchant: other_merchant, order_date: date)
+
+      result = order_query_service.orders(date, date_week_ago)
+
+      expect(result).to be_empty
+    end
+
+    it 'does not return orders outside the date range' do
+      create(:order, merchant: merchant, order_date: date - 8.days)
+
+      result = order_query_service.orders(date, date_week_ago)
+
+      expect(result).to be_empty
+    end
+  end
+end
