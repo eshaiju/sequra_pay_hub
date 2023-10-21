0068c3a6df91f3dafa915c108a5e9f48873f3f6b
Shaiju Edakulangara <eshaiju@gmail.com>
2023-10-21 18:44:23 +0530

    Add disbursement creation scheduler

--- spec/sidekiq_schedule_spec.rb
+++ spec/sidekiq_schedule_spec.rb
@@ -3,11 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Sidekiq Schedule' do
+  let(:schedule_file) { Rails.root.join('config', 'sidekiq_scheduler.yml') }
+  let(:schedule) { YAML.load_file(schedule_file) }
+
   it 'has the correct schedule for CreateMonthlyFeesJob' do
-    schedule_file = Rails.root.join('config', 'sidekiq_scheduler.yml')
-
-    schedule = YAML.load_file(schedule_file)
-
     expect(schedule['calculate_monthly_fee_job']).not_to be_nil
 
     expect(schedule['calculate_monthly_fee_job']['class']).to eq('CreateMonthlyFeesJob')
@@ -15,5 +14,14 @@ RSpec.describe 'Sidekiq Schedule' do
     expect(schedule['calculate_monthly_fee_job']['queue']).to eq('default')
     expect(schedule['calculate_monthly_fee_job']['description'])
       .to eq('Calculate monthly fees for the previous month at the beginning of each month')
+  end
+
+  it 'has the correct schedule for CreateDisbursementsJob' do
+    expect(schedule['create_disbursements_job']).not_to be_nil
+    expect(schedule['create_disbursements_job']['class']).to eq('CreateDisbursementsJob')
+    expect(schedule['create_disbursements_job']['cron']).to eq('0 8 * * *')
+    expect(schedule['create_disbursements_job']['queue']).to eq('default')
+    expect(schedule['create_disbursements_job']['description'])
+      .to eq('Create disbursements for eligible merchants by 8:00 AM UTC')
   end
 end
